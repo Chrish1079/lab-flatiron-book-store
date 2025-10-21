5560259bd57065b4688e57b0d66e5898907fd945
Chrish1079 <itssuper1079@gmail.com>
2025-10-21 00:59:58 -0400

    submit

--- index.js
+++ index.js
@@ -45,3 +45,26 @@ const bookStore = {
 
 // Write your code here!
 
+const bookStoreTitle = document.getElementById('header')
+bookStoreTitle.textContent = bookStore.name
+
+const deleteThis = document.getElementById('delete-this')
+if (deleteThis) {
+    deleteThis.remove()
+}
+const bookList = document.getElementById('book-list')
+
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement('li');
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('p');
+    const bookImage = document.createElement('img');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+    bookImage.alt = `${book.title} cover`;
+    
+    bookContainer.append(bookTitle, bookAuthor, bookImage);
+    bookList.appendChild(bookContainer);
+});
